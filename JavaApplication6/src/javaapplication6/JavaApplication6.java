64c901cdf19f0e72056d5c1b61e917e8d5dd45df
zbossik <zbossik@gmail.com>
2015-03-20 18:43:15 +0400

    aaaa

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -15,5 +15,7 @@ public class JavaApplication6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello worker");
     }
 }
